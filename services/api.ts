44624ccea9d692650c61c553f7a24c0ba925ca69
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-09-14 13:41:13 -0300

    Recuperando Estado da Autenticação

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,11 @@
 import axios from 'axios';
+import {parseCookies} from 'nookies';
+
+const cookies = parseCookies()
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333/' // é preciso estar rodando a aplicação do auth backend feita em node.
+  baseURL: 'http://localhost:3333/', // é preciso estar rodando a aplicação do auth backend feita em node.
+  headers: {  
+    Authorization: `Bearer ${cookies['nextauth.token']}`
+  }
 })
